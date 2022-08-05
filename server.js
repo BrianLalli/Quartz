8e74d60419f71c341a0f9f6e553ea732e94b9d06
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-08-05 18:01:29 -0500

    I GOT IT WORKING Y'ALLgit add .git add .git add .git add .git add .1

--- server.js
+++ server.js
@@ -1,64 +1,46 @@
+const path = require('path');
 const express = require('express');
-const dotenv = require('dotenv').config();
-const PORT = process.env.PORT || 5000;
-const colors = require('colors')
-const app = express();
-const path = require('path')
-const session = require("express-session");
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-const Sequelize = require("sequelize");
-const connect = require("connect");
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL Username
-    user: 'root',
-    // TODO: Add MySQL Password
-    password: '',
-    database: 'projects_db'
-  },
-  console.log(`Connected to the projects_db database.`)
-);
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3301
-});
-const SequelizeStore = require("connect-session-sequelize")(
-  connect.session.Store
-);
+const helpers = require('./utils/helpers');
+const sequelize = require('./backend/config/connection.js');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const colors = require('colors')
 
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Set up sessions with cookies using SQL as storage
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-    maxAge: 21600000, //changed to 6 hours
+    maxAge: 21600000,
   },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize,
+    db: sequelize
   })
 };
 
 app.use(session(sess));
-// use handlebars 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars');
-// app.engine('handlebars', hbs.engine);
 
-// Express middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //send api calls to router
 app.use('/api/Projects', require('./backend/routes/projectRoutes'));
 // app.use('api/Users', require('./routes/userRoutes'));
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
 
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'.bgGreen +' http://localhost:3001/'.rainbow));
+});
 
